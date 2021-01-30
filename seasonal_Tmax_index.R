4dfd42c7c4c58d05fa5c5cd30d83d68e1cc7ef0e
karthik k murthy <78032500+karthikmurthy85@users.noreply.github.com>
2021-01-30 07:48:41 +0530

    Update seasonal_Tmax_index.R

--- seasonal_Tmax_index.R
+++ seasonal_Tmax_index.R
@@ -4,14 +4,12 @@ library(rgdal)
 tmp_seas_xtrme_index <- function(tmp_val, qq){
   at1 <- 0
   v1 <- tmp_val
-  v1[v1 > 0] <- 0
   v2 <- c(0, v1, 0)
   for(i in 1:12){
     mn1 <- v1[seq(i, length(v1), 12)]
     options(digits = 10)
-    mnx <- (-1*quantile(abs(mn1[mn1 < 0]),
-                        probs=c(qq), na.rm=T))
-    mnx1 <- v2[seq(i, length(v1), 12)[which(mn1 < mnx)]+1]
+    mnx <- quantile(mn1,probs=c(qq), na.rm=T)
+    mnx1 <- v2[seq(i, length(v1), 12)[which(mn1 > mnx)]+1]
     if(length(mnx1) > 0){
       for(j in 1:length(mnx1)){
         poly <- Polygon(data.frame(y = c(mnx, mnx1[j], mnx), x = c(1:3)))
@@ -25,20 +23,20 @@ tmp_seas_xtrme_index <- function(tmp_val, qq){
 }
 
 
-d1 <- read.csv('/media/karthik/ADATA HD720/global_veg/datasets/NDVIt_equi_ncycl_ts.csv',
+d1 <- read.csv('D:/global_veg/datasets/NDVIt_equi_ncycl_ts.csv',
                h=F)
 colnames(d1) <- c('id','nodata','x','y', 'Ncycle','CC','aic', 'Radj',
                   'model_arb', 'model_CC_aic', 'model_Radj',
                   'mn_rg', 'mn_rr', 'mn_rrt', 'mn_rtp', 'rtn',
                   'sd_rg', 'sd_rr', 'sd_rrt', 'sd_rtp')
 
-tmpmx <- brick('global_veg/rasters/CRU_Tmax.tif')
+tmpmx <- brick('D:/global_veg/rasters/CRU_Tmax.tif')
 
 library(foreach)
 library(doParallel)
-registerDoParallel(4)
+registerDoParallel(8)
 foreach(i = 1:nrow(d1))%dopar%{
-  library(reshape2);library(rgeos)
+  library(reshape2);library(rgeos); library(raster)
   cord <- matrix(as.numeric(d1[i, 3:4]), ncol=2)
   tmpdt <- melt(extract(tmpmx, cord))
   
@@ -46,7 +44,7 @@ foreach(i = 1:nrow(d1))%dopar%{
   tmp_indx095 <- tmp_seas_xtrme_index(tmpdt$value, 0.95)
   
   tmp_indx_dat <- matrix(c(i,cord,tmp_indx090,tmp_indx095), byrow=T, nrow = 1)
-  path <- '/media/karthik/ADATA HD720/global_veg/datasets/Tmax_extr_seasonal_new_dat.csv'
+  path <- 'D:/global_veg/datasets/Tmax_extr_seasonal_new_dat.csv'
   write.table(tmp_indx_dat, path, sep=',', 
               append=TRUE, row.names=FALSE, col.names=FALSE)
 }
